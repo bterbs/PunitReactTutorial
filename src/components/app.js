a9bc0c9c02bd3c064763ac0115ecaeb630c79f08
B Terbush <brittanyterbush@gmail.com>
2018-02-27 10:37:19 -0800

    Create new restaurant components as children of App component

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
+import Restaurant from './restaurant'
+import RestaurantSelectButtons from '.restaurant_select_buttons'
 
 export default class App extends Component {
   render() {
     return (
-      <div>React simple starter</div>
+      <div>
+        <Restaurant />
+        <RestaurantSelectButtons /> 
+      </div>
     );
   }
 }
--- src/components/restaurant.js
+++ src/components/restaurant.js
@@ -1,6 +1,6 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-export default class Restaurant {
+export default class Restaurant extends Component {
   render() {
     return (
       <div> This is the restaurant component </div>
